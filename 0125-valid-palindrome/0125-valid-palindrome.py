1e138a1ee814d37bd4f5665aa6fbeaeda0718d13
yoonseopkim <qc70045@gmail.com>
2023-07-03 18:21:21 +0900

    Time: 49 ms (96.59%), Space: 18 MB (14.94%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -3,8 +3,5 @@ class Solution:
         import re
         s = s.lower()
         s = re.sub(r"[^\uAC00-\uD7A3a-zA-Z0-9]", "", s)
-
-        for i in range(len(s)//2):
-            if s[i] != s[-1 - i]:
-                return False
-        return True
+        s_reverse = s[::-1]
+        return s == s_reverse
