2fc56d5db74abaa6780218d1319aa802410338ff
yoonseopkim <qc70045@gmail.com>
2023-07-13 11:23:08 +0900

    Time: 50 ms (35.67%), Space: 16.4 MB (17.43%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -10,8 +10,6 @@ class Solution:
             return True
         elif p is None or q is None:
             return False
+
         else:
             return (p.val == q.val) and self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
-            
-       
-            
